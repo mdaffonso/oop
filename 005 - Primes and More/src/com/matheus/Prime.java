13a7e14bf65306896c250941c495533f59480fdf
mdaffonso <mdaffonso@gmail.com>
2021-08-12 16:08:25 -0300

    prime 2

--- 005 - Primes and More/src/com/matheus/Prime.java
+++ 005 - Primes and More/src/com/matheus/Prime.java
@@ -48,6 +48,7 @@ public class Prime {
 
     public boolean isPrime(int number) {
         if(number <= 1) return false;
+        if(number == 2) return true;
         if(number % 2 == 0) return false;
         for(int i = 3; i <= number/2; i+=2) {
             if(number % i == 0) return false;
